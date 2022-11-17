c172636bd3caf31d82e971c96912fdfe36d44725
Andrey_Rodionov <andrey_36rus@mail.ru>
2022-11-17 18:44:47 +0300

    task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args){
 
+        Human Max = new Human( " Максим " ,"Минск", 1988," бренд-менеджер");
+        System.out.println(Max.toString());
+        Human Anna= new Human("Аня ","Москва ", 1993," методист образовательных программ");
+        System.out.println(Anna.toString());
+        Human Katia= new Human("Катя ","Калининград ", 1992," продакт-менеджером");
+        System.out.println(Katia.toString());
+        Human Artem= new Human("Артем ","Москва ", 1995," директор по развитию бизнеса");
+        System.out.println(Artem.toString());
+
+
     }
-}
+
+
+
+            }
+
+
+
---
+++ task2/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        Car Lada=new Car(" Lada","Granta",2015,"России ","желтого",1.7);
+        System.out.println(Lada.toString());
+        Car Audi=new Car(" Audi", "A8 50 L TDI quattro",2020,"Германии ","черный",3.0);
+        System.out.println(Audi.toString());
+        Car BMW=new Car(" BMW","Z8",2021,"Германии","черный ",3.0);
+        System.out.println(BMW.toString());
+        Car Kia=new Car(" Kia","Sportage",2018,"Южной Корее","красный",2.4);
+        System.out.println(Kia.toString());
+        Car Hyundai=new Car(" Hyundai","Avante",2016,"Южной Корее","оранжевый",1.6);
+        System.out.println(Hyundai.toString());
+
+
+    }
+}
+
+
+
+
+
